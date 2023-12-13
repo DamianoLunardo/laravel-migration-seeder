7ed34cd634444e4e83d612d5c237d733a69e48cb
DamianoL <damianolunardo@hotmail.it>
2023-12-13 15:55:39 +0100

    Add table

---
+++ database/migrations/2023_12_13_144804_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza', 100);
+            $table->string('stazione_di_arrivo', 100);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->date('data_di_partenza');
+            $table->unsignedTinyInteger('numero_carrozze')->nullable();
+            $table->boolean('in_orario')->nullable();
+            $table->boolean('cancellato')->nullable();
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
