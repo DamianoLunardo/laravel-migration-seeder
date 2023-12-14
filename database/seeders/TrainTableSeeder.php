fe27a99e483e3ea53e7b9e4389448d9107525a41
DamianoL <damianolunardo@hotmail.it>
2023-12-14 17:06:18 +0100

    Fix date

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -21,8 +21,8 @@ class TrainTableSeeder extends Seeder
             $newTrain->stazione_di_arrivo = $faker->city();
             $newTrain->orario_di_partenza = $faker->time();
             $newTrain->orario_di_arrivo = $faker->time();
-            $newTrain->data_di_partenza = $faker->dateTimeBetween('now', '+3 month');
-            $newTrain->data_di_arrivo = $faker->dateTimeBetween('data_di_partenza', '+3 hour');
+            $newTrain->data_di_partenza = $faker->dateTimeBetween('now', '+10 days');
+            $newTrain->data_di_arrivo = $faker->dateTimeBetween('+10 days', '+11 days');
             $newTrain->numero_carrozze = $faker->numberBetween(1, 10);
             $newTrain->in_orario = $faker->randomElement([false, true]);
             $newTrain->cancellato = $faker->randomElement([false, true]);
